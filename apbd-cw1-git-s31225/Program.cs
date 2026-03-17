a1e6bd5b874851664eb99b717495df3308a39185
Rafał Święcicki <s31225@pjwstk.edu.pl>
2026-03-17 18:48:13 +0100

    feat: change message on feature branch

--- apbd-cw1-git-s31225/Program.cs
+++ apbd-cw1-git-s31225/Program.cs
@@ -1,6 +1,6 @@
 ﻿using apbd_cw1_git_s31225;
 
-Console.WriteLine("Hello, World!"); // Exercise 1 done
+Console.WriteLine("Conflict from feature branch!"); // Exercise 1 done
 
 Console.WriteLine("Starting StatisticsHelper...");
 int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
