d1e4e4df2ea6713d83203c437de8fd182073de94
Rafał Święcicki <s31225@pjwstk.edu.pl>
2026-03-17 18:23:47 +0100

    feat: add CalculateMax method

--- apbd-cw1-git-s31225/StatisticsHelper.cs
+++ apbd-cw1-git-s31225/StatisticsHelper.cs
@@ -15,4 +15,21 @@ public class StatisticsHelper
         }
         return (double) sum / nums.Length;
     }
+
+    public static double CalculateMax(int[] values)
+    {
+        if (values == null || values.Length == 0)
+        {
+            throw new ArgumentException("Array cannot be null or empty!");
+        }
+        int max = values[0];
+        for (int i = 0; i < values.Length; i++)
+        {
+            if (values[i] > max)
+            {
+                max = values[i];
+            }
+        }
+        return max;
+    }
 }
