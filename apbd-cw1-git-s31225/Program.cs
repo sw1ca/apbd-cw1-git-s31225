409aa9601938fc42138ea09811bd18bb29bf3be6
Rafał Święcicki <s31225@pjwstk.edu.pl>
2026-03-17 19:01:35 +0100

    feat: add CalculateMax and CalculateMin methods call

--- apbd-cw1-git-s31225/Program.cs
+++ apbd-cw1-git-s31225/Program.cs
@@ -10,3 +10,11 @@ Console.WriteLine("Starting StatisticsHelper...");
 int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 double average = StatisticsHelper.CalculateAverage(array);
 Console.WriteLine("Average value: " + average);
+
+Console.WriteLine("Starting CalculateMax...");
+double max = StatisticsHelper.CalculateMax(array);
+Console.WriteLine("Max value: " + max);
+
+Console.WriteLine("Starting CalculateMin...");
+double min = StatisticsHelper.CalculateMin(array);
+Console.WriteLine("Max value: " + min);
